1c7bf661e94286387aa65ad96f095e7e60acb0a3
takewiki <hulilei@takewiki.com.cn>
2022-12-13 13:31:48 +0800

    6.1

--- modules/mdlMfgCostCom/mdlMfgCostCom_ui.R
+++ modules/mdlMfgCostCom/mdlMfgCostCom_ui.R
@@ -4,11 +4,7 @@ tabItem(tabName = 'mdlMfgCostCom',
                              tabBox(title ="产品成本工作台",width = 12,
                                     id="tabSet_mdlMfgCostCom",height = '300px',
                                     #注册UI组件
-                                    mdlMngrCostUI::mfgCostUI()
-                                    
-                                     
-                                    
-                                    
+                                    mdlMfgCostUI::mfgCostUI()
                                     
                                     
                              )
